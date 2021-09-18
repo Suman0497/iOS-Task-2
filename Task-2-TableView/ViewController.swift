6ff0f8d8e02c21ebd640b52250d1b5516c844c67
Suman <sumanreshmj@gmail.com>
2021-09-18 16:27:49 +0530

    Initial Commit

---
+++ Task-2-TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task-2-TableView
+//
+//  Created by apple on 18/09/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
